0823d22377c2cdc75f926382ce81e7a403b79848
Md. Shakhawat Hossain Shahin <shakhawat.ch@gmail.com>
2022-04-30 15:04:52 +0600

    Assets added ✅

---
+++ ShaktiCoin/Core/Pin/View/PinView.swift
@@ -0,0 +1,20 @@
+//
+//  PinView.swift
+//  ShaktiCoin
+//
+//  Created by Shakhawat Hossain Shahin on 30/4/22.
+//
+
+import SwiftUI
+
+struct PinView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct PinView_Previews: PreviewProvider {
+    static var previews: some View {
+        PinView()
+    }
+}
---
+++ ShaktiCoin/Extensions/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  ShaktiCoin
+//
+//  Created by Shakhawat Hossain Shahin on 30/4/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let browDark = Color("BrownDarkColor")
+    let brownLight = Color("BrownLightColor")
+    let brownOutline = Color("BrownOutlineColor")
+}
