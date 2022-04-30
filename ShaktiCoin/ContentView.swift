cc3af20fc3ddb368b7cdf555f6723240464b0bd6
Md. Shakhawat Hossain Shahin <shakhawat.ch@gmail.com>
2022-04-30 14:26:43 +0600

    Initial Commit

---
+++ ShaktiCoin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShaktiCoin
+//
+//  Created by Shakhawat Hossain Shahin on 30/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShaktiCoin/ShaktiCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaktiCoinApp.swift
+//  ShaktiCoin
+//
+//  Created by Shakhawat Hossain Shahin on 30/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ShaktiCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
